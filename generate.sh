c15c3c96fd79dbd02dce5a594f74ff0f283e9b03
Maxime Tricoire <max.tricoire@gmail.com>
2014-10-23 16:06:11 +0200

    update docker container image

--- generate.sh
+++ generate.sh
@@ -3,4 +3,4 @@
 sudo rm -rf haxe-generated/cpp/* haxe-generated/java/* haxe-generated/js/*
 
 # generate files using maxleiko/haxe Docker container
-docker run --rm -v `pwd`:/root/workspace:rw -t maxleiko/haxe:dev
+docker run --rm -v `pwd`:/root/workspace:rw -t maxleiko/haxe

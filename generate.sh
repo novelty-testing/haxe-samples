5508d77b35bad0f5561c7210efc62134a6f58f1a
Maxime Tricoire <max.tricoire@gmail.com>
2014-10-23 16:04:57 +0200

    update build

---
+++ generate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# rm -rf old generated files
+sudo rm -rf haxe-generated/cpp/* haxe-generated/java/* haxe-generated/js/*
+
+# generate files using maxleiko/haxe Docker container
+docker run --rm -v `pwd`:/root/workspace:rw -t maxleiko/haxe:dev
